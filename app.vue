be50292a4173b10f901f5abdf746e5b24929964a
DaniyelMe <DaniyelMenda@Gmail.com>
2023-11-10 15:49:59 +0200

    feat: adds  base dynamic table

--- app.vue
+++ app.vue
@@ -1,5 +1,28 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Table :rows="rows" :columns="columns" />
   </div>
 </template>
+
+<script setup lang="ts">
+import Table from './components/Table.vue';
+
+const rows = [
+  {
+    date: new Date().toLocaleString(),
+    name: 'Alice',
+    address: '123 Maple Street',
+  },
+  {
+    date: new Date().toLocaleString(),
+    name: 'Bob',
+    address: '456 Oak Avenue',
+  },
+  {
+    date: new Date().toLocaleString(),
+    name: 'Charlie',
+    address: '789 Pine Lane',
+  },
+];
+const columns = ['date', 'name', 'address'];
+</script>
---
+++ components/Table.vue
@@ -0,0 +1,45 @@
+<template>
+  <table class="basic-table">
+    <thead>
+      <tr>
+        <th v-for="column in columns" :key="column">{{ column }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="row in rows" :key="row.id">
+        <td v-for="(value, key) in row" :key="key">
+          <slot :name="key" :value="value">{{ value }}</slot>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  columns: string[];
+  rows: Record<string, string | number>[];
+}>();
+</script>
+
+<style lang="less" scoped>
+.basic-table {
+  th {
+    border-bottom: 1px solid var(--secondary-bg-color);
+    text-transform: capitalize;
+  }
+
+  th,
+  td {
+    text-align: left;
+    padding: 8px;
+  }
+
+  tbody tr:nth-child(odd) {
+  }
+
+  tbody tr:nth-child(even) {
+    background-color: var(--secondary-bg-color);
+  }
+}
+</style>
